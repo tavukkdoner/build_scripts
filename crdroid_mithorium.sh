85fc86f7c9857a83ee28ac1c88a5e07cb4702e74
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2024-07-04 19:05:51 +0300

    Create crdroid_mithorium.sh

---
+++ crdroid_mithorium.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Remove Local Manifests
+rm -rf .repo/local_manifests/ 
+
+# Init Rom Manifest
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a14-avc1-viperfx .repo/local_manifests 
+
+# git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a14-avc1-viperfx .repo/local_manifests && 
+# if [ ! $? == 0 ]
+# then   curl -o .repo/local_manifests https://github.com/tavukkdoner/local_manifests.git
+#  echo "Git clone failed, downloading through curl instead..."
+# fi 
+
+# Sync the repositories
+/opt/crave/resync.sh 
+ 
+# Set up build environment
+export BUILD_USERNAME=tavukkdoner 
+export BUILD_HOSTNAME=crave 
+
+source build/envsetup.sh
+ 
+# Build the ROM
+lunch lineage_Mi439_4_19-ap2a-userdebug && make installclean && mka bacon
