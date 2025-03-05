6d6490514d14541ff2718212ade5764d2eb2a7c4
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-03-05 08:31:11 +0200

    Update crdroid_mithorium15-1.sh

--- crdroid_mithorium15-1.sh
+++ crdroid_mithorium15-1.sh
@@ -56,6 +56,10 @@ fi
 # lunch lineage_Mi439_4_19-ap3a-eng && make installclean && mka bacon
 
 # https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/411251
-lunch lineage_Mi439_4_19-ap4a-userdebug && make installclean && mka bacon
+# lunch lineage_Mi439_4_19-ap4a-userdebug && make installclean && mka bacon
 # lunch lineage_Mi439_4_19-ap4a-eng && make installclean && mka bacon
 
+# https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-15.0.0_r23
+# compatibility_matrix.5.xml is ok
+lunch lineage_Mi439_4_19-bp1a-userdebug && make installclean && mka bacon
+# lunch lineage_Mi439_4_19-bp1a-eng && make installclean && mka bacon
