38957134df1e039edbe8e26081c389773e23265d
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-04-26 19:44:20 +0300

    Update crdroid_mithorium15.sh

--- crdroid_mithorium15.sh
+++ crdroid_mithorium15.sh
@@ -2,7 +2,7 @@
 
 # Remove Local Manifests
 rm -rf .repo/local_manifests/ 
-#rm -rf frameworks/base
+# rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
 
