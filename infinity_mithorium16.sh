70ec0abec39efe9f3f377a4afc2e899f5c5e4827
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-07-30 04:46:38 +0300

    Update infinity_mithorium16.sh

--- infinity_mithorium16.sh
+++ infinity_mithorium16.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Remove Local Manifests
-rm -rf .repo/local_manifests/ 
-rm -rf prebuilts/clang/host/linux-x86
+#rm -rf .repo/local_manifests/ 
+#rm -rf prebuilts/clang/host/linux-x86
 # rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
 # rm -rf hardware/qcom-caf/msm8937
 
 # Init Rom Manifest
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+#repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
-git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-infinity .repo/local_manifests
+#git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-infinity .repo/local_manifests
 
 # Original local_manifest Mi439 A15 QPR2 no modifications -> a15-qpr2-mithorium
 # git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a15-qpr2-mithorium .repo/local_manifests
@@ -24,7 +24,7 @@ git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-fi
 # fi 
 
 # Sync the repositories  
-/opt/crave/resync.sh 
+#/opt/crave/resync.sh 
 # /opt/crave/resynctest.sh
 
 
@@ -43,11 +43,11 @@ source build/envsetup.sh
 export WITH_GMS=false
 export TARGET_BOOT_ANIMATION_RES=720
 
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-cd vendor/infinity-priv/keys
-chmod +x keys.sh
-./keys.sh
-cd ../../../
+#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+#cd vendor/infinity-priv/keys
+#chmod +x keys.sh
+#./keys.sh
+#cd ../../../
 
 # https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-15.0.0_r23
 # https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/421399
@@ -55,7 +55,7 @@ cd ../../../
 # lunch lineage_Mi439_4_19-bp1a-eng && make installclean && mka bacon
 
 # https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/433445
-lunch infinity_Mi439_4_19-bp2a-userdebug && make installclean && mka bacon
+lunch infinity_Mi439_4_19-userdebug && make installclean && mka bacon
 # lunch lineage_Mi439_4_19-bp2a-eng && make installclean && mka bacon
 
 #brunch Mi439_4_19 userdebug
