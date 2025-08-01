df6dbfbb4ff8a898341f0d5bdf0e88748120eb5b
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-08-01 17:03:00 +0300

    Update infinity_mithorium16.sh

--- infinity_mithorium16.sh
+++ infinity_mithorium16.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Remove Local Manifests
-#rm -rf .repo/local_manifests/ 
-#rm -rf prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests/ 
+rm -rf prebuilts/clang/host/linux-x86
 # rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
 # rm -rf hardware/qcom-caf/msm8937
 
 # Init Rom Manifest
-#repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
-#git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-infinity .repo/local_manifests
+git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-infinity .repo/local_manifests
 
 # Original local_manifest Mi439 A15 QPR2 no modifications -> a15-qpr2-mithorium
 # git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a15-qpr2-mithorium .repo/local_manifests
@@ -24,11 +24,9 @@
 # fi 
 
 # Sync the repositories  
-#/opt/crave/resync.sh 
+/opt/crave/resync.sh 
 # /opt/crave/resynctest.sh
 
-rm -rf system/sepolicy
-git clone https://github.com/ProjectInfinity-X/system_sepolicy -b 16 system/sepolicy
 # Set up build environment
 export BUILD_USERNAME=tavukkdoner 
 export BUILD_HOSTNAME=crave
@@ -45,11 +43,11 @@ export WITH_GMS=false
 export TARGET_BOOT_ANIMATION_RES=720
 export WITH_GAPPS=false
 
-#git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
-#cd vendor/infinity-priv/keys
-#chmod +x keys.sh
-#./keys.sh
-#cd ../../../
+git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
+cd vendor/infinity-priv/keys
+chmod +x keys.sh
+./keys.sh
+cd ../../../
 
 # https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-15.0.0_r23
 # https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/421399
