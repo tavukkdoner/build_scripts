aa9f42967995e95ac1a613e83dc376ba31aa7043
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-08-02 06:56:48 +0300

    Create evolution_b2q.sh

---
+++ evolution_b2q.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Remove Local Manifests
+rm -rf .repo/local_manifests/ 
+rm -rf prebuilts/clang/host/linux-x86
+# rm -rf frameworks/base
+# rm -rf kernel/xiaomi/mithorium-4.19/kernel
+# rm -rf hardware/mithorium/
+# rm -rf hardware/qcom-caf/msm8937
+# rm -rf prebuilts/clang/host/
+
+# Init Rom Manifest
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+
+# Clone local_manifests repository
+git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-b2q .repo/local_manifests
+
+# Sync the repositories  
+/opt/crave/resync.sh 
+# /opt/crave/resynctest.sh
+
+# Set up build environment
+export BUILD_USERNAME=tavukkdoner 
+export BUILD_HOSTNAME=crave
+
+source build/envsetup.sh
+
+if [ ! -e "vendor/lineage-priv" ]; then
+    curl -O https://raw.githubusercontent.com/tavukkdoner/crDroid-build-signed-script/crdroid/create-signed-env.sh
+    chmod +x create-signed-env.sh
+    ./create-signed-env.sh
+fi
+
+# https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-15.0.0_r23
+# https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/421399
+# lunch lineage_Mi439_4_19-bp1a-userdebug && make installclean && mka bacon
+# lunch lineage_Mi439_4_19-bp1a-eng && make installclean && mka bacon
+
+export WITH_GMS=false
+export TARGET_USES_MINI_GAPPS=false
+# https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/433445
+lunch lineage_b2q-bp2a-userdebug && make installclean && m evolution
+# lunch lineage_Mi439_4_19-bp2a-eng && make installclean && m evolution
