64cc03fb9de94e7743b7ffe5beda3564fdb7d9be
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-08-14 07:18:11 +0300

    Update axion_m16.sh

--- axion_m16.sh
+++ axion_m16.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Remove Local Manifests
-# rm -rf .repo/local_manifests/ 
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests/ 
+rm -rf prebuilts/clang/host/linux-x86
 # rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
 # rm -rf hardware/qcom-caf/msm8937
 
 # Init Rom Manifest
-# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
 
 # Clone local_manifests repository
-# git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-axion .repo/local_manifests
+git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-axion .repo/local_manifests
 
 # Original local_manifest Mi439 A15 QPR2 no modifications -> a15-qpr2-mithorium
 # git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a15-qpr2-mithorium .repo/local_manifests
