571a1bc3c20e9772046571b01bb3dd8a5e779ff2
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-08-18 07:15:33 +0300

    Update crdroid_m16.sh

--- crdroid_m16.sh
+++ crdroid_m16.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Remove Local Manifests
-rm -rf .repo/local_manifests/ 
-rm -rf prebuilts/clang/host/linux-x86
+#rm -rf .repo/local_manifests/ 
+#rm -rf prebuilts/clang/host/linux-x86
 # rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
@@ -12,7 +12,7 @@ rm -rf prebuilts/clang/host/linux-x86
 repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
 
 # Clone local_manifests repository
-git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final .repo/local_manifests
+#git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final .repo/local_manifests
 
 # Original local_manifest Mi439 A15 QPR2 no modifications -> a15-qpr2-mithorium
 # git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a15-qpr2-mithorium .repo/local_manifests
