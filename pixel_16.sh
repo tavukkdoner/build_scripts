8c8d7f3f4a14d9b74e0797cbadd07985a29e25d9
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-09-19 04:48:02 +0300

    Update pixel_16.sh

--- pixel_16.sh
+++ pixel_16.sh
@@ -2,7 +2,7 @@
 
 # Remove Local Manifests
 rm -rf .repo/local_manifests/ 
-rm -rf vendor/lineage-priv
+#rm -rf vendor/lineage-priv
 #rm -rf prebuilts/clang/host/linux-x86
 #rm -rf packages/modules/adb
 #rm -rf kernel/xiaomi/mithorium-4.19
