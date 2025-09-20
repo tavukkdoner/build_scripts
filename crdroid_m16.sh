628a22637a622ecfd484dd9b1c6e3440cf2ee9bc
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-09-21 00:13:23 +0300

    Update crdroid_m16.sh

--- crdroid_m16.sh
+++ crdroid_m16.sh
@@ -3,8 +3,8 @@
 # Remove Local Manifests
 rm -rf .repo/local_manifests/ 
 # rm -rf prebuilts/clang/host/linux-x86
-rm -rf packages/modules/adb
-rm -rf kernel/xiaomi/mithorium-4.19
+#rm -rf packages/modules/adb
+#rm -rf kernel/xiaomi/mithorium-4.19
 # rm -rf hardware/mithorium/
 # rm -rf hardware/qcom-caf/msm8937
 
