d01b3bf5f36de94e9cea72c077455b5f0d9ed1df
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-01-30 17:58:05 +0200

    Update infinity_mithorium16.sh

--- infinity_mithorium16.sh
+++ infinity_mithorium16.sh
@@ -9,7 +9,7 @@ rm -rf prebuilts/clang/host/linux-x86
 # rm -rf hardware/qcom-caf/msm8937
 
 # Init Rom Manifest
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16-QPR2 -g default,-mips,-darwin,-notdefault
 
 # Clone local_manifests repository
 git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-final-infinity .repo/local_manifests
@@ -26,6 +26,22 @@ git clone https://github.com/tavukkdoner/local_manifests.git --depth 1 -b a16-fi
 # Sync the repositories  
 /opt/crave/resync.sh 
 # /opt/crave/resynctest.sh
+
+cd frameworks/av
+git fetch https://github.com/LineageOS/android_frameworks_av refs/changes/14/468714/1 && git cherry-pick FETCH_HEAD
+cd ../..
+
+cd frameworks/native
+curl https://github.com/VoltageOS/frameworks_native/commit/7e4df9a63981d6796d31e810a427b7d58c0d4dc7.patch | git am
+cd ../..
+
+#cd vendor/lineage
+#curl https://github.com/tavukkdoner/android_vendor_crdroid/commit/56cfbd73169367e57d5a8aed169b0ac89eb41958.patch | git am
+#cd ../..
+
+cd device/lineage/sepolicy
+curl https://github.com/tavukkdoner/android_device_crdroid_sepolicy/commit/b4eec83467aa3bfd7473ebcac9a6424bf10075c7.patch | git am
+cd ../../..
 
 # Set up build environment
 export BUILD_USERNAME=tavukkdoner 
@@ -52,7 +68,7 @@ export TARGET_BOOT_ANIMATION_RES=720
 export WITH_GAPPS=false
 export TARGET_ENABLE_BLUR=true
 export UCLAMP_FEATURE_ENABLED=false
-export TARGET_USES_VULKAN=true
+export TARGET_USES_VULKAN=false
 
 if [ ! -e "vendor/infinity-priv" ]; then
     git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys-template vendor/infinity-priv/keys
