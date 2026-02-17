bef03e47b38dc186798e06937886073bb2bffd86
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-02-17 06:40:20 +0200

    Update pixel_16.sh

--- pixel_16.sh
+++ pixel_16.sh
@@ -60,7 +60,7 @@ export BUILD_HOSTNAME=crave
 export TARGET_ENABLE_BLUR=true
 export TARGET_USES_VULKAN=false
 export UCLAMP_FEATURE_ENABLED=false
-export TARGET_USES_MAGICPORTRAIT=false
+export TARGET_USES_MAGICPORTRAIT=true
 
 source build/envsetup.sh
 
