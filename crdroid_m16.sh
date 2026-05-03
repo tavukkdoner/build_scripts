ae88e482c1f8e8a69b948ea17694b63e11d43cc6
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-05-03 06:46:31 +0300

    Update crdroid_m16.sh

--- crdroid_m16.sh
+++ crdroid_m16.sh
@@ -2,7 +2,7 @@
 
 # Remove Local Manifests
 rm -rf .repo/local_manifests/ 
-rm -rf prebuilts/clang/host/linux-x86
+#rm -rf prebuilts/clang/host/linux-x86
 #rm -rf external/chromium-webview
 #rm -rf packages/modules/adb
 #rm -rf kernel/xiaomi/mithorium-4.19
@@ -59,7 +59,7 @@ export BUILD_HOSTNAME=crave
 export TARGET_ENABLE_BLUR=true
 export TARGET_USES_VULKAN=false
 export UCLAMP_FEATURE_ENABLED=false
-export LTO_FEATURE_ENABLED=false
+export LTO_FEATURE_ENABLED=true
 export TARGET_USES_MAGICPORTRAIT=true
 export TARGET_USES_EROFS2=true
 
