d0995dcfb7e0eea424908d25b35442ff1536f675
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2026-05-12 06:50:45 +0300

    Update axion_m16.sh

--- axion_m16.sh
+++ axion_m16.sh
@@ -2,7 +2,7 @@
 
 # Remove Local Manifests
 rm -rf .repo/local_manifests/ 
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 # rm -rf frameworks/base
 # rm -rf kernel/xiaomi/mithorium-4.19/kernel
 # rm -rf hardware/mithorium/
